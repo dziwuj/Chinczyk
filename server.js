071030614693003decdeeb8c0ec257107dcd594e
Zbyszko <bylo@poczta.onet.pl>
2021-05-03 10:05:26 +0200

    Procfile add

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const store = new session.MemoryStore();
 
 // const room = require("./routes/room")
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 let rooms = [[]]
 let rooms_inter = []
